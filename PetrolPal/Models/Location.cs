d0e2e1414f8dfb7bdeee5e01c3e7a2f85f7c3c66
SirForez <orrigudlaugur@gmail.com>
2015-05-24 05:52:23 +0000

    Specific trip view implemented, trips now have starting and ending location, and specific cost. As well, the specific trip view links to google maps marked with both locations and the best road to take between them.

---
+++ PetrolPal/Models/Location.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetrolPal.Models
+{
+    public class Location
+    {
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+    }
+}
--- PetrolPal/Models/Trip.cs
+++ PetrolPal/Models/Trip.cs
@@ -8,9 +8,12 @@ namespace PetrolPal.Models
     public class Trip
     {
         public double fuelUsed { get; set; }
+        public double cost { get; set; }
         public int km { get; set; }
         //Remember that we need to select time as DateTime then parse to this format of string: yyyy-MM-dd HH:mm
         public string startTime { get; set; }
         public string endTime { get; set; }
+        public Location startLocation { get; set; }
+        public Location endLocation { get; set; }
     }
 }
