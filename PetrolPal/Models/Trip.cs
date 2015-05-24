65caa00822c4487419124821155a8b879cb5f2f5
saraa12 <saraa12@ru.is>
2015-05-24 01:18:20 +0000

    Added date pickers and submit button on TripTable site

--- PetrolPal/Models/Trip.cs
+++ PetrolPal/Models/Trip.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,11 @@ namespace PetrolPal.Models
     public class Trip
     {
         public double fuelUsed { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true,
+               DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime startTime { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true,
+               DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime endTime { get; set; }
     }
 }
