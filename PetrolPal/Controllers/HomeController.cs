b655039dc08feba37ed4fed162397ac2edae895e
SirForez <orrigudlaugur@gmail.com>
2015-05-24 00:01:32 +0000

    DataService implemented, Home/Index() sort of implemented

--- PetrolPal/Controllers/HomeController.cs
+++ PetrolPal/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PetrolPal.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,7 +9,35 @@ namespace PetrolPal.Controllers
 {
     public class HomeController : Controller
     {
+        public DataService ds = new DataService();
+        public string regnumber { get; set; }
+        
+        [HttpGet]
         public ActionResult Index()
+        {
+            ViewBag.Message = "Your application home page."
+
+            if (regnumber != null)
+            {
+                return RedirectToAction("TripTable");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Index(IndexViewModel model)
+        {
+            ViewBag.Message = "Your application home page."
+
+            regnumber = model.regnum;
+            ds.regnumber = regnumber;
+            return RedirectToAction("TripTable");
+        }
+
+        public ActionResult TripTable()
         {
             return View();
         }
--- PetrolPal/Models/ManageViewModels.cs
+++ PetrolPal/Models/ManageViewModels.cs
@@ -7,6 +7,8 @@ namespace PetrolPal.Models
 {
     public class IndexViewModel
     {
+        public string regnum { get; set; }
+
         public bool HasPassword { get; set; }
         public IList<UserLoginInfo> Logins { get; set; }
         public string PhoneNumber { get; set; }
